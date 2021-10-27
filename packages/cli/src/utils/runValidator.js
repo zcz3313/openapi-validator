642e29a8d99ef068af105c31a5ab8f806b8d4368
András Csányi <andras.csanyi@pm.me>
2021-10-27 19:09:49 +0200

    tmp

--- packages/cli/src/utils/runValidator.js
+++ packages/cli/src/utils/runValidator.js
@@ -1,8 +1,15 @@
-const { Spectral } = require('@stoplight/spectral-core');
+const OpenApiValidator = require('@acsanyi-test/openapi-validator-core/src/openapi-validator');
 
 const processInput = async function(program) {
-  const spectral = new Spectral();
-  spectral.setRuleset()
+  
+  let args = program.args;
+  
+  if (args.length === 0){
+    program.help();
+    return Promise.reject(2);
+  }
+  
+  const openApiValidatorCore = new OpenApiValidator.Builder();
 }
 
 module.exports = processInput;
